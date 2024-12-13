03a1041b4151961b84e48b9dd077c4751d8e7c5f
Kaizeno44 <nguyensbo@gmail.com>
2024-12-13 10:18:42 +0700

    CNPM project

--- CNPM/app/urls.py
+++ CNPM/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path("", views.home),
-]
+    path("", views.home,name='home'),
+    path('login/', views.login_view, name='login'),  
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- CNPM/app/views.py
+++ CNPM/app/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'app/home.html')
+def login_view(request):
+    return render(request, 'app/login.html')
